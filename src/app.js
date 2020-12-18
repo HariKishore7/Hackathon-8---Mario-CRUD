11b84bc2c187d316d61ac627fcb65283b5b730c8
HariKishore7 <harikishoret99@gmail.com>
2020-12-18 11:20:00 +0530

    changed some part of code

--- src/app.js
+++ src/app.js
@@ -29,12 +29,13 @@ app.get('/mario/:id',async(req,res)=>{
 
 app.post("/mario",async(req,res)=>{
     const newMario=req.body;
-    if(newMario.name || Number(newMario.weight)){
-        res.status(400).send({message:"either name or weight is missing"});
-    }else{
+    if(newMario.name && Number(newMario.weight)){
         const newMarioDocument=new marioModel(newMario);
         await newMarioDocument.save();
         res.status(201).send(newMarioDocument);
+    }else{
+res.status(400).send({message:"either name or weight is missing"});
+        
     }
 });
 
@@ -43,15 +44,15 @@ app.patch("/mario/:id",async(req,res)=>{
     const newMario=req.body;
     try{
         const existingMarioDoc=await marioModel.findById(id);
-        if(newMario.name && Number(newMario.weight)){
+        if(!newMario.name && !Number(newMario.weight)){
                 res.status(400).send({message:"both name and weight is missing"});
             }
         else{
-            if(!newMario.name){
+            if(newMario.name){
                 existingMarioDoc.name=newMario.name;
             }
-            if(!Number(newMario.weight)){
-                existingMarioDoc.weight=Number(newMario.weight);
+            if((newMario.weight)){
+                existingMarioDoc.weight=(newMario.weight);
             }
             await existingMarioDoc.save();
             res.send(existingMarioDoc);
@@ -60,7 +61,7 @@ app.patch("/mario/:id",async(req,res)=>{
     catch(e){
         res.status(400).send({message:e.message});
     }
-})
+});
 
 app.delete("/mario/:id",async(req,res)=>{
     const id=req.params.id;
