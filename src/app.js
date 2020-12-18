5a9b25af24838fa02fb7f4092122eba40c20b26f
HariKishore7 <harikishoret99@gmail.com>
2020-12-18 10:51:22 +0530

    updated again

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('/mario/:id',async(req,res)=>{
 
 app.post("/mario",async(req,res)=>{
     const newMario=req.body;
-    if((newMario.name)||(newMario.weight)){
+    if(newMario.name || number(newMario.weight)){
         res.status(400).send({message:"either name or weight is missing"});
     }else{
         const newMarioDocument=new marioModel(newMario);
@@ -43,15 +43,14 @@ app.patch("/mario/:id",async(req,res)=>{
     const newMario=req.body;
     try{
         const existingMarioDoc=await marioModel.findById(id);
-        if((newMario.name) && 
-            (newMario.weight)){
+        if(newMario.name && Number(newMario.weight)){
                 res.status(400).send({message:"both name and weight is missing"});
             }
         else{
-            if(!(newMario.name)){
+            if(!newMario.name){
                 existingMarioDoc.name=newMario.name;
             }
-            if(!(newMario.weight)){
+            if(!Number(newMario.weight)){
                 existingMarioDoc.weight=newMario.weight;
             }
             await existingMarioDoc.save();
